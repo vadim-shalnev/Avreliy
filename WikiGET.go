c52912bef9a27209189d4f80c452aead3f5a2977
Vadim <vadim@Ubuntu.myguest.virtualbox.org>
2023-12-30 22:59:42 +0300

    split for 3 files

--- WikiGET.go
+++ WikiGET.go
@@ -10,7 +10,6 @@ import (
 
 func WikiGET() string {
 	resp, err := http.Get("https://ru.wikipedia.org/wiki/%D0%9F%D0%BE%D1%80%D1%82%D0%B0%D0%BB:%D0%A4%D0%B8%D0%BB%D0%BE%D1%81%D0%BE%D1%84%D0%B8%D1%8F")
-
 	if err != nil {
 		fmt.Println("Ошибка в GET запросе:", err)
 		os.Exit(1)
@@ -63,6 +62,7 @@ func GetTextContent(n *html.Node) string {
 	if n.Type == html.TextNode {
 		// Текстовый узел
 		textContent = n.Data
+		textContent += "\n"
 	} else {
 		// Рекурсивный вызов для дочерних узлов
 		for c := n.FirstChild; c != nil; c = c.NextSibling {
--- main.go
+++ main.go
@@ -3,44 +3,16 @@ package main
 import (
 	"bufio"
 	"context"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/jinzhu/gorm"
 	"log"
 	"os"
-	"strings"
-
-	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
-var (
-	// Menu texts
-	firstMenu  = "<b>Menu 1</b>\n\nA beautiful menu with a shiny inline button."
-	secondMenu = "<b>Menu 2</b>\n\nA better menu with even more shiny inline buttons."
-
-	// Button texts
-	nextButton     = "Next"
-	backButton     = "Back"
-	tutorialButton = "Tutorial"
-
-	// Store bot screaming status
-	screaming = false
-	bot       *tgbotapi.BotAPI
-
-	// Keyboard layout for the first menu. One button, one row
-	firstMenuMarkup = tgbotapi.NewInlineKeyboardMarkup(
-		tgbotapi.NewInlineKeyboardRow(
-			tgbotapi.NewInlineKeyboardButtonData(nextButton, nextButton),
-		),
-	)
-
-	// Keyboard layout for the second menu. Two buttons, one per row
-	secondMenuMarkup = tgbotapi.NewInlineKeyboardMarkup(
-		tgbotapi.NewInlineKeyboardRow(
-			tgbotapi.NewInlineKeyboardButtonData(backButton, backButton),
-		),
-		tgbotapi.NewInlineKeyboardRow(
-			tgbotapi.NewInlineKeyboardButtonURL(tutorialButton, "https://core.telegram.org/bots/api"),
-		),
-	)
-)
+type User struct {
+	gorm.Model
+	ChatID int64 `gorm:"unique_index"`
+}
 
 func main() {
 	var err error
@@ -64,7 +36,7 @@ func main() {
 	updates := bot.GetUpdatesChan(u)
 
 	// Pass cancellable context to goroutine
-	go receiveUpdates(ctx, updates)
+	go ReceiveUpdates(ctx, updates)
 
 	// Tell the user the bot is online
 	log.Println("Start listening for updates. Press enter to stop")
@@ -73,133 +45,4 @@ func main() {
 	bufio.NewReader(os.Stdin).ReadBytes('\n')
 	cancel()
 
-}
-
-func receiveUpdates(ctx context.Context, updates tgbotapi.UpdatesChannel) {
-	// `for {` means the loop is infinite until we manually stop it
-	for {
-		select {
-		// stop looping if ctx is cancelled
-		case <-ctx.Done():
-			return
-		// receive update from channel and then handle it
-		case update := <-updates:
-			handleUpdate(update)
-		}
-	}
-}
-
-func handleUpdate(update tgbotapi.Update) {
-	switch {
-	// Handle messages
-	case update.Message != nil:
-		handleMessage(update.Message)
-		break
-
-	// Handle button clicks
-	case update.CallbackQuery != nil:
-		handleButton(update.CallbackQuery)
-		break
-	}
-}
-
-func handleMessage(message *tgbotapi.Message) {
-	user := message.From
-	text := message.Text
-
-	if user == nil {
-		return
-	}
-
-	// Print to console
-	log.Printf("%s wrote %s", user.FirstName, text)
-
-	var err error
-	if strings.HasPrefix(text, "/") {
-		err = handleCommand(message.Chat.ID, text)
-	} else if screaming && len(text) > 0 {
-		msg := tgbotapi.NewMessage(message.Chat.ID, strings.ToUpper(text))
-		// To preserve markdown, we attach entities (bold, italic..)
-		msg.Entities = message.Entities
-		_, err = bot.Send(msg)
-	} else {
-		// This is equivalent to forwarding, without the sender's name
-		copyMsg := tgbotapi.NewCopyMessage(message.Chat.ID, message.Chat.ID, message.MessageID)
-		_, err = bot.CopyMessage(copyMsg)
-	}
-
-	if err != nil {
-		log.Printf("An error occured: %s", err.Error())
-	}
-	// Вызываем бота
-	TsCitat := WikiGET()
-	sendText(user.ID, TsCitat)
-}
-
-// Ответ бота
-func sendText(who int64, what string) {
-	sm := tgbotapi.NewMessage(who, what)
-
-	_, err := bot.Send(sm)
-	if err != nil {
-		panic(err)
-	}
-	/*
-		_, err = bot.Send(tgbotapi.NewMessage(who, what))
-		if err != nil {
-			panic(err)
-		}
-	*/
-}
-
-// When we get a command, we react accordingly
-func handleCommand(chatId int64, command string) error {
-	var err error
-
-	switch command {
-	case "/scream":
-		screaming = true
-		break
-
-	case "/whisper":
-		screaming = false
-		break
-
-	case "/menu":
-		err = sendMenu(chatId)
-		break
-	}
-
-	return err
-}
-
-func handleButton(query *tgbotapi.CallbackQuery) {
-	var text string
-
-	markup := tgbotapi.NewInlineKeyboardMarkup()
-	message := query.Message
-
-	if query.Data == nextButton {
-		text = secondMenu
-		markup = secondMenuMarkup
-	} else if query.Data == backButton {
-		text = firstMenu
-		markup = firstMenuMarkup
-	}
-
-	callbackCfg := tgbotapi.NewCallback(query.ID, "")
-	bot.Send(callbackCfg)
-
-	// Replace menu text and keyboard
-	msg := tgbotapi.NewEditMessageTextAndMarkup(message.Chat.ID, message.MessageID, text, markup)
-	msg.ParseMode = tgbotapi.ModeHTML
-	bot.Send(msg)
-}
-
-func sendMenu(chatId int64) error {
-	msg := tgbotapi.NewMessage(chatId, firstMenu)
-	msg.ParseMode = tgbotapi.ModeHTML
-	msg.ReplyMarkup = firstMenuMarkup
-	_, err := bot.Send(msg)
-	return err
 }
